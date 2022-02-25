bda38424f95d996d81db94b42a3b052ba1397c5f
U039b <git@0x39b.fr>
2022-02-25 16:25:52 +0100

    Fix minor issues

--- build.sh
+++ build.sh
@@ -15,11 +15,13 @@ find . -maxdepth 1 -type f -name "pirogue-*" -exec mv {} dist/ \;
 
 cd dist
 
-dpkg-scanpackages --multiversion . > Packages
-gzip -k -f Packages
+# dpkg-scanpackages --multiversion . > Packages
+# gzip -k -f Packages
 
-apt-ftparchive release . > Release
-gpg --default-key "6D49D146B9D61919B96044C43A5B3B14BD040926" -abs -o - Release > Release.gpg
-gpg --default-key "6D49D146B9D61919B96044C43A5B3B14BD040926" --clearsign -o - Release > InRelease
+# apt-ftparchive release . > Release
+# gpg --default-key "6D49D146B9D61919B96044C43A5B3B14BD040926" -abs -o - Release > Release.gpg
+# gpg --default-key "6D49D146B9D61919B96044C43A5B3B14BD040926" --clearsign -o - Release > InRelease
+
+# gpg --armor --export "6D49D146B9D61919B96044C43A5B3B14BD040926" > Key.gpg
 
 cd ..
