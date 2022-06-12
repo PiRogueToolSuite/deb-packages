bf31ef424b2e8af284e9137150f2b02ab96293ad
U039b <git@0x39b.fr>
2022-06-12 13:08:39 +0200

    Update the crappy build script

--- build.sh
+++ build.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 set -e
 
+# Build packages
 for source in `find . -maxdepth 1 -type d -name "pirogue-*"`; do
     cd $source;
     case "$source" in
@@ -17,20 +18,20 @@ for source in `find . -maxdepth 1 -type d -name "pirogue-*"`; do
     cd ..
 done
 
+# Collect packages
 rm -rf dist/pirogue
 mkdir -p dist/pirogue
 dcmd --deb mv *changes dist/pirogue 
 
-cd dist/pirogue
+# Copy them into the ppa
+cp dist/pirogue/*.deb ../ppa/pirogue/
+cd ../ppa/pirogue
+
+# Rebuild the ppa
+rm -f Packages* Release*
 apt-ftparchive packages . > Packages
 gzip -k -f Packages
 apt-ftparchive release . > Release.tmp
 mv Release.tmp Release
-
 gpg --local-user "6D49D146B9D61919B96044C43A5B3B14BD040926" -abs -o - Release > Release.gpg
 
-cd ../..
-
-cd dist
-gpg --armor --export "6D49D146B9D61919B96044C43A5B3B14BD040926" > Key.gpg
-cd ..
